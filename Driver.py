136c8d0aad64ceab92efdbb1834b40878d94aa31
joedegere <degerej@hartwick.edu>
2020-02-19 14:22:54 -0500

    File updated!

---
+++ Driver.py
@@ -0,0 +1,12 @@
+from nodes import SingleLinkedList
+
+jesus = SingleLinkedList()
+print(jesus)
+
+jesus.prepend(23)
+jesus.prepend(40)
+jesus.prepend(22)
+jesus.prepend(44)
+jesus.prepend('Hello')
+
+print(jesus)
---
+++ nodes.py
@@ -0,0 +1,28 @@
+# Joe Degere
+# 2/19/2020
+# Link List Homework
+
+
+class ListNode:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+    def __repr__(self):
+        return repr(self.data)
+
+
+class SingleLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __repr__(self):
+        nodes = []
+        curr = self.head
+        while curr:
+            nodes.append(repr(curr))
+            curr = curr.next
+        return '[' + ','.join(nodes) + ']'
+
+    def prepend(self, data):
+        self.head = ListNode(data=data, next=self.head)
